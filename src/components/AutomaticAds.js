1036ff2cb851e0cf78c183ec62aae341b55fdff3
dominik <dongron@wp.pl>
2019-03-28 22:13:47 +0700

    Automatic ads, different way to push credentials

--- src/components/AutomaticAds.js
+++ src/components/AutomaticAds.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react'
 
 class AutomaticAds extends Component {
   componentDidMount() {
-    let adsbygoogle = window.adsbygoogle || []
-    adsbygoogle.push({
+    window.adsbygoogle = window.adsbygoogle || []
+    window.adsbygoogle.push({
       google_ad_client: 'ca-pub-8118457789375955',
       enable_page_level_ads: true,
     })
